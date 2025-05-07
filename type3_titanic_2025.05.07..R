4f5593661fd239f175174d406bf337428bc1c8f2
greimas1 <occult183183@gmail.com>
2025-05-07 21:21:38 +0900

    Update type3_titanic_2025.05.07..R

--- type3_titanic_2025.05.07..R
+++ type3_titanic_2025.05.07..R
@@ -46,11 +46,11 @@ md <- glm(Gender~SibSp + Parch + Fare, data = rdata, family = "binomial")
 summary(md)
 result2 <- summary(md)$coefficients["Parch","Estimate"]
 print(round(result2,3))
-#-0.579
+#-0.579 (05.06.일자 코딩 결과와 답이 다름, 시험환경에서 어려움.. 이상하게 초기화 해서 실행해도 잘 되지 않음)
 
 result3 <- summary(md)$coefficients["SibSp","Estimate"]
 print(round(exp(result3),3))
-#1.004
+#1.004 (05.06.일자 코딩 결과와 답이 다름, 시험환경에서 어려움.. 이상하게 초기화 해서 실행해도 잘 되지 않음)
 
 
 
